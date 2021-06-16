694430d3d9c37fe9a58534183391a615258feae8
Vitaly Potenko <potenkov@gmail.com>
2021-06-16 15:09:01 +0300

    Initial commit: bootstrapped the project.

---
+++ README.md
@@ -0,0 +1,4 @@
+# The "Payman": students payments management system
+
+This is a legacy project implemented inside Moodle LMS (https://moodle.org) and now being in need for refactoring out to become a standalone PHP web application.
+The project is built using the Domain Driven Design and framework-independence approaches. Its purpose is to learn how much the DDD can be beneficial, and how much it's possible to keep the core application code distant from a PHP framework. 
---
+++ bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * This file is part of the vtos/payment application.
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ *
+ * @copyright 2021 Vitaly Potenko <potenkov@gmail.com>
+ * @license https://www.gnu.org/licenses/gpl-3.0.html GNU GPL v3 or later
+ * @link https://github.com/vtos/payman GitHub
+ */
+
+declare(strict_types=1);
+
+require_once __DIR__ . '/vendor/autoload.php';
